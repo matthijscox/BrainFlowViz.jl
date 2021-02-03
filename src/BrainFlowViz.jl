d9c74b015f866b603809fdb0c541ab91962df6ae
matthijscox <matthijs.cox@gmail.com>
2021-02-03 20:57:07 +0100

    bottom axes streaming start

--- src/BrainFlowViz.jl
+++ src/BrainFlowViz.jl
@@ -2,6 +2,14 @@ module BrainFlowViz
 
     using BrainFlow
     using Makie, GLMakie, AbstractPlotting
+    using DataStructures
+
+    function calc_avg_band_powers(data, board_id = BrainFlow.SYNTHETIC_BOARD)
+        chans = 1:size(data, 2)
+        sampling_rate = BrainFlow.get_sampling_rate(board_id)
+        apply_filter = true
+        avg_band_power = BrainFlow.get_avg_band_powers(transpose(data), chans, sampling_rate, apply_filter)
+    end
 
     function set_dark!(ax)
         ax.backgroundcolor[] = :black
@@ -32,9 +40,9 @@ module BrainFlowViz
         color = :green,
         ncolumns = 2,
         column_gap = 0,
-        layout_size = (1200, 700),
-        plot_band_powers = true,
+        layout_size = (1200, 700)
     )
+
         nsamples, nchannels = size(ys)
         ys_n = []
         for n = 1:nchannels
@@ -81,21 +89,55 @@ module BrainFlowViz
         colgap!(layout_top, column_gap)
         layout[1,1] = layout_top
 
-        # add a nested grid at the bottom
-        if plot_band_powers
-            layout_bottom = GridLayout()
-            bottom_axes = [LAxis(scene)]
+        return scene, layout, ys_n
+    end
+
+    function init_bandpowers!(
+        scene::Scene, 
+        layout::GridLayout; 
+        theme = :light, 
+        color = :green, 
+        kwargs...
+    )
+        
+        n_bands = 5
+        
+        bottom_axes = [LAxis(scene) for _ in 1:n_bands]
+
+        layout_bottom = GridLayout()
+        if theme == :dark
             set_dark!(bottom_axes)
-            layout_bottom[1,:] = bottom_axes
-            layout[2,1] = layout_bottom
-            rowsize!(layout, 2, Relative(0.3))
+        end
+        for n = 1:n_bands
+            layout_bottom[1,n] = bottom_axes[n]
+        end
+        layout[2,1] = layout_bottom
+        rowsize!(layout, 2, Relative(0.3))
+
+        bandpower_buffer_size = 100
+        xs = 1:bandpower_buffer_size
+        ys_n = []
+
+        for n = 1:n_bands
+            cb = CircularBuffer{Float64}(bandpower_buffer_size)
+            append!(cb, rand(bandpower_buffer_size)) 
+            push!(ys_n, Node(cb))
+            lines!(bottom_axes[n], xs, ys_n[n], color = color, linewidth = 2)
         end
 
-        return scene, ys_n
+        return ys_n
     end
 
     # live plotting function
-    function plot_data(data_func::Function, nsamples, nchannels; delay = 0.05, y_lim = :auto, kwargs...)
+    function plot_data(
+        data_func::Function, 
+        nsamples::Int, 
+        nchannels::Int; 
+        delay = 0.05, 
+        y_lim = :auto, 
+        plot_band_powers::Bool = true, 
+        kwargs...
+    )
 
         xs = collect(1:nsamples)
         #ys = zeros(nsamples, nchannels)
@@ -105,7 +147,12 @@ module BrainFlowViz
             y_lim = (minimum(ys), maximum(ys))
         end
 
-        scene, ys_n = init_scene(xs, ys; y_lim = y_lim, kwargs...)
+        scene, layout, ys_n = init_scene(xs, ys; y_lim = y_lim, kwargs...)
+
+        # add a nested grid at the bottom
+        if plot_band_powers
+            bands_data = init_bandpowers!(scene, layout; kwargs...)
+        end
 
         display(scene)
 
@@ -124,6 +171,16 @@ module BrainFlowViz
             # is this the most efficient? Or could we refresh 4 axes in a scene at the same time
             for n = 1:nchannels
                 ys_n[n][] = view(ys, :, n)
+            end
+
+            if plot_band_powers
+                n_bands = length(bands_data)
+                #TODO: calc_avg_band_powers(ys) # also add board_id...
+                for n = 1:n_bands
+                    cb = bands_data[n][]
+                    push!(cb, rand(1)[1])
+                    bands_data[n][] = cb
+                end
             end
 
             # optional if you want to zoom in + update limits:
