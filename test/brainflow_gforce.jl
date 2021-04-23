224942036a12d56e09894b0c0e23155bcdf0237d
matthijscox <matthijs.cox@gmail.com>
2021-04-23 20:01:02 +0200

    nchannel update

--- test/brainflow_gforce.jl
+++ test/brainflow_gforce.jl
@@ -1,11 +1,11 @@
 using BrainFlow
 using BrainFlowViz
 
-function get_some_board_data(board_shim, nsamples)
+function get_some_board_data(board_shim, nsamples, nchannels=8)
     data = BrainFlow.get_current_board_data(nsamples, board_shim)
     data = transpose(data)
-    emg_data = view(data, :, 2:9)
-    for chan in 1:8
+    emg_data = view(data, :, 2:(1+nchannels))
+    for chan in 1:nchannels
         emg_channel_data = view(emg_data, :, chan)
         BrainFlow.detrend(emg_channel_data, BrainFlow.CONSTANT)
     end
@@ -15,20 +15,20 @@ end
 ### Start streaming
 BrainFlow.enable_dev_logger(BrainFlow.BOARD_CONTROLLER)
 params = BrainFlowInputParams()
-board_shim = BrainFlow.BoardShim(BrainFlow.GFORCE_PRO_BOARD, params)
+board_shim = BrainFlow.BoardShim(BrainFlow.GFORCE_DUAL_BOARD, params)
 BrainFlow.prepare_session(board_shim)
 BrainFlow.start_stream(board_shim)
 
-nchannels = 8
+nchannels = 2
 nsamples = 512
-data_func = ()->get_some_board_data(board_shim, nsamples)
+data_func = ()->get_some_board_data(board_shim, nsamples, nchannels)
 
 # start a live plotting task
 t = @task BrainFlowViz.plot_data(
     data_func, 
     nsamples, 
     nchannels; 
-    y_lim = [-1300 1300], 
+    #y_lim = [-1300 1300], 
     theme = :dark,
     color = :lime,
     )
